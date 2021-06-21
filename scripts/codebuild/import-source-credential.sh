ad88b90ddedc84e333b1c23ccbf7397c40a749fa
dengmingtong <dengmingtong@hotmail.com>
2021-06-21 21:24:55 +0800

    change codebuild script

--- scripts/codebuild/import-source-credential.sh
+++ scripts/codebuild/import-source-credential.sh
@@ -5,8 +5,10 @@ set -e
 GIT_ACCESS_TOKEN=$1
 GITHUB_USER=$2
 
+echo "Start to import source credentials to codebuild"
 aws codebuild import-source-credentials \
     --server-type GITHUB \
     --auth-type PERSONAL_ACCESS_TOKEN \
     --token $GIT_ACCESS_TOKEN \
     --username $GITHUB_USER
+echo "Import source credentials to codebuild successfully"
