60ca9d85c4a2f7dadce5e52a6031e7d6911fe37f
liuyong <23968560@qq.com>
2021-06-22 09:20:59 +0800

    add sync sample data to S3

--- scripts/create-offline.sh
+++ scripts/create-offline.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-cd ./codebuild
+curr_dir=$(pwd)
+
+Stage=$1
+if [[ -z $Stage ]];then
+  Stage='dev-workshop'
+fi
+
+echo "Stage=$Stage"
+
+echo "1. ========= sync sample data to S3 =============="
+cd ${curr_dir}/../sample-data
+./sync_data_to_s3.sh
+
+echo "2. ========= Create codebuild =============="
+cd ${curr_dir}/codebuild
 ./register-to-codebuild-offline.sh
 
