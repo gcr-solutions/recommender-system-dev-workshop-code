64483a722f7d6a651a1fc1c154747e27e4858014
dengmingtong <dengmingtong@hotmail.com>
2021-06-23 18:54:24 +0800

    change istio ingress gateway name

--- scripts/load-seed-data.sh
+++ scripts/load-seed-data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-dns_name=$(kubectl get svc istio-ingressgateway -n istio-system -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+dns_name=$(kubectl get svc istio-ingressgateway-news-dev -n istio-system -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 
 echo "endpoint: $dns_name"
 
