bc30930957d91a7c978665c81ed9698fa584dbb2
liuyong <23968560@qq.com>
2021-06-25 10:04:06 +0800

    add recall config change

---
+++ scripts/change-recall-config-offline.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -e
+
+Stage=$1
+if [[ -z $Stage ]];then
+  Stage='dev-workshop'
+fi
+
+if [[ -z $REGION ]]; then
+ export REGION='ap-northeast-1'
+fi
+
+echo "Stage=$Stage"
+echo "REGION=$REGION"
+
+AWS_CMD="aws"
+if [[ -n $PROFILE ]]; then
+  AWS_CMD="aws --profile $PROFILE"
+fi
+
+AWS_ACCOUNT_ID=$($AWS_CMD sts get-caller-identity --region ${REGION} --query Account --output text)
+
+echo "AWS_ACCOUNT_ID: ${AWS_ACCOUNT_ID}"
+
+BUCKET=aws-gcr-rs-sol-${Stage}-${REGION}-${AWS_ACCOUNT_ID}
+PREFIX=sample-data-news
+
+ls -l ../sample-data/notification/inverted-list/recall_config_v2.pickle
+
+$AWS_CMD s3 cp ../sample-data/notification/inverted-list/recall_config_v2.pickle s3://${BUCKET}/${PREFIX}/notification/inverted-list/recall_config.pickle
+
+echo "Start notifying online service"
+
+NOTIFY_STEP_FUNC=arn:aws:states:${REGION}:${AWS_ACCOUNT_ID}:stateMachine:rs-dev-workshop-News-NotificationStepFunc
+
+echo "start-execution ${NOTIFY_STEP_FUNC} ..."
+
+$AWS_CMD stepfunctions start-execution --state-machine-arn ${NOTIFY_STEP_FUNC} \
+--input "{ \"change_type\": \"item-new\", \"Bucket\":  \"${BUCKET}\", \"S3Prefix\": \"${PREFIX}\" }"
+
+echo "Done"
+
