e03b3030db85b8b34c0778b0ae60959855ae5330
jjin-2019 <lijin2019@hotmail.com>
2021-07-05 02:58:27 +0000

    finish create-personalize.sh

--- scripts/create-personalize.sh
+++ scripts/create-personalize.sh
@@ -2,9 +2,11 @@
 set -e
 
 #create dataset group
-#datasetGroupArn=$(aws personalize create-dataset-group --name GCR-RS-News-Dataset-Group --output text)
-#echo "dataset_Group_Arn: ${datasetGroupArn}"
-datasetGroupArn="arn:aws:personalize:ap-northeast-1:466154167985:dataset-group/GCR-RS-News-Dataset-Group"
+datasetGroupArn=$(aws personalize create-dataset-group --name GCR-RS-News-Dataset-Group --output text)
+echo "dataset_Group_Arn: ${datasetGroupArn}"
+
+# for test
+#datasetGroupArn="arn:aws:personalize:ap-northeast-1:466154167985:dataset-group/GCR-RS-News-Dataset-Group"
 
 
 # #delete exist schema
@@ -13,49 +15,47 @@ datasetGroupArn="arn:aws:personalize:ap-northeast-1:466154167985:dataset-group/G
 # aws personalize delete-schema --schema-arn "arn:aws:personalize:ap-northeast-1:466154167985:schema/NewsItemSchema"
 # aws personalize delete-schema --schema-arn "arn:aws:personalize:ap-northeast-1:466154167985:schema/NewsInteractionSchema"
 
-# sleep 10
+sleep 10
 
-# #create schema
-# echo "creating Schema..."
-# user_schema_arn=$(aws personalize create-schema \
-# 	--name NewsUserSchema \
-# 	--schema file://./personalize/NewsUserSchema.json --output text)
+#create schema
+echo "creating Schema..."
+user_schema_arn=$(aws personalize create-schema \
+	--name NewsUserSchema \
+	--schema file://./personalize/NewsUserSchema.json --output text)
 
-# item_schema_arn=$(aws personalize create-schema \
-# 	--name NewsItemSchema \
-# 	--schema file://./personalize/NewsItemSchema.json --output text)
+item_schema_arn=$(aws personalize create-schema \
+	--name NewsItemSchema \
+	--schema file://./personalize/NewsItemSchema.json --output text)
 
-# interaction_schema_arn=$(aws personalize create-schema \
-# 	--name NewsInteractionSchema \
-# 	--schema file://./personalize/NewsInteractionSchema.json --output text)
+interaction_schema_arn=$(aws personalize create-schema \
+	--name NewsInteractionSchema \
+	--schema file://./personalize/NewsInteractionSchema.json --output text)
 
-# #delete dataset
 
-# #create dataset
-# echo "create dataset..."
-# user_dataset_arn=$(aws personalize create-dataset \
-# 	--name NewsUserDataset \
-# 	--dataset-group-arn ${datasetGroupArn} \
-# 	--dataset-type Users \
-# 	--schema-arn ${user_schema_arn} --output text)
+#create dataset
+echo "create dataset..."
+user_dataset_arn=$(aws personalize create-dataset \
+	--name NewsUserDataset \
+	--dataset-group-arn ${datasetGroupArn} \
+	--dataset-type Users \
+	--schema-arn ${user_schema_arn} --output text)
 
-# item_dataset_arn=$(aws personalize create-dataset \
-# 	--name NewsItemDataset \
-# 	--dataset-group-arn ${datasetGroupArn} \
-# 	--dataset-type Items \
-# 	--schema-arn ${item_schema_arn} --output text)
+item_dataset_arn=$(aws personalize create-dataset \
+	--name NewsItemDataset \
+	--dataset-group-arn ${datasetGroupArn} \
+	--dataset-type Items \
+	--schema-arn ${item_schema_arn} --output text)
 	
-# interaction_dataset_arn=$(aws personalize create-dataset \
-# 	--name NewsInteractionDataset \
-# 	--dataset-group-arn ${datasetGroupArn} \
-# 	--dataset-type Interactions \
-# 	--schema-arn ${interaction_schema_arn} --output text)
+interaction_dataset_arn=$(aws personalize create-dataset \
+	--name NewsInteractionDataset \
+	--dataset-group-arn ${datasetGroupArn} \
+	--dataset-type Interactions \
+	--schema-arn ${interaction_schema_arn} --output text)
 
-#for test
-user_dataset_arn="arn:aws:personalize:ap-northeast-1:466154167985:dataset/GCR-RS-News-Dataset-Group/USERS"
-item_dataset_arn="arn:aws:personalize:ap-northeast-1:466154167985:dataset/GCR-RS-News-Dataset-Group/ITEMS"
-interaction_dataset_arn="arn:aws:personalize:ap-northeast-1:466154167985:dataset/GCR-RS-News-Dataset-Group/INTERACTIONS"
-
+# #for test
+# user_dataset_arn="arn:aws:personalize:ap-northeast-1:466154167985:dataset/GCR-RS-News-Dataset-Group/USERS"
+# item_dataset_arn="arn:aws:personalize:ap-northeast-1:466154167985:dataset/GCR-RS-News-Dataset-Group/ITEMS"
+# interaction_dataset_arn="arn:aws:personalize:ap-northeast-1:466154167985:dataset/GCR-RS-News-Dataset-Group/INTERACTIONS"
 
 
 #Get Bucket Name
@@ -85,76 +85,76 @@ PERSONALIZE_ROLE_BUILD=arn:aws:iam::${AWS_ACCOUNT_ID}:role/gcr-rs-${Stage}-perso
 echo "PERSONALIZE_ROLE_BUILD=${PERSONALIZE_ROLE_BUILD}"
 echo "Check if your personalize role arn is equal to the PERSONALIZE_ROLE_BUILD. If not, please follow the previous step to create iam role for personalize!"
 
-# echo "\nWaiting for creating dataset finishing...\n"
-# sleep 60
+echo "\nWaiting for creating dataset finishing...\n"
+sleep 60
 
-# #create import job
-# user_dataset_import_job_arn=$(aws personalize create-dataset-import-job \
-#   --job-name NewsUserImportJob \
-#   --dataset-arn ${user_dataset_arn} \
-#   --data-source dataLocation=s3://${BUCKET_BUILD}/sample-data-news/system/personalize-data/personalize_user.csv \
-#   --role-arn ${PERSONALIZE_ROLE_BUILD} \
-#   --output text)
+#create import job
+user_dataset_import_job_arn=$(aws personalize create-dataset-import-job \
+  --job-name NewsUserImportJob \
+  --dataset-arn ${user_dataset_arn} \
+  --data-source dataLocation=s3://${BUCKET_BUILD}/sample-data-news/system/personalize-data/personalize_user.csv \
+  --role-arn ${PERSONALIZE_ROLE_BUILD} \
+  --output text)
   
   
-# item_dataset_import_job_arn=$(aws personalize create-dataset-import-job \
-#   --job-name NewsItemImportJob \
-#   --dataset-arn ${item_dataset_arn} \
-#   --data-source dataLocation=s3://${BUCKET_BUILD}/sample-data-news/system/personalize-data/personalize_item.csv \
-#   --role-arn ${PERSONALIZE_ROLE_BUILD} \
-#   --output text)
+item_dataset_import_job_arn=$(aws personalize create-dataset-import-job \
+  --job-name NewsItemImportJob \
+  --dataset-arn ${item_dataset_arn} \
+  --data-source dataLocation=s3://${BUCKET_BUILD}/sample-data-news/system/personalize-data/personalize_item.csv \
+  --role-arn ${PERSONALIZE_ROLE_BUILD} \
+  --output text)
   
-# interaction_dataset_import_job_arn=$(aws personalize create-dataset-import-job \
-#   --job-name NewsInteractionImportJob \
-#   --dataset-arn ${interaction_dataset_arn} \
-#   --data-source dataLocation=s3://${BUCKET_BUILD}/sample-data-news/system/personalize-data/personalize_interactions.csv \
-#   --role-arn ${PERSONALIZE_ROLE_BUILD} \
-#   --output text)
+interaction_dataset_import_job_arn=$(aws personalize create-dataset-import-job \
+  --job-name NewsInteractionImportJob \
+  --dataset-arn ${interaction_dataset_arn} \
+  --data-source dataLocation=s3://${BUCKET_BUILD}/sample-data-news/system/personalize-data/personalize_interactions.csv \
+  --role-arn ${PERSONALIZE_ROLE_BUILD} \
+  --output text)
  
   
 # #for test
 # user_dataset_import_job_arn="arn:aws:personalize:ap-northeast-1:466154167985:dataset-import-job/NewsUserImportJob"
 # item_dataset_import_job_arn="arn:aws:personalize:ap-northeast-1:466154167985:dataset-import-job/NewsItemImportJob"
 
-# #monitor import job
-# echo "Data Importing... It will takes no longer than 10 min..."
-# MAX_TIME=10*60 # 10 min
-# CURRENT_TIME=0
-# while(( ${CURRENT_TIME} < ${MAX_TIME} )) 
-# do
-#     user_dataset_import_job_status=$(aws personalize describe-dataset-import-job \
-#                 --dataset-import-job-arn ${user_dataset_import_job_arn} | jq '.datasetImportJob.status')
-#     item_dataset_import_job_status=$(aws personalize describe-dataset-import-job \
-#                 --dataset-import-job-arn ${item_dataset_import_job_arn} | jq '.datasetImportJob.status')
-#     interaction_dataset_import_job_status=$(aws personalize describe-dataset-import-job \
-#                 --dataset-import-job-arn ${interaction_dataset_import_job_arn} | jq '.datasetImportJob.status')
+#monitor import job
+echo "Data Importing... It will takes no longer than 10 min..."
+MAX_TIME=`expr 10 \* 60` # 10 min
+CURRENT_TIME=0
+while(( ${CURRENT_TIME} < ${MAX_TIME} )) 
+do
+    user_dataset_import_job_status=$(aws personalize describe-dataset-import-job \
+                --dataset-import-job-arn ${user_dataset_import_job_arn} | jq '.datasetImportJob.status' -r)
+    item_dataset_import_job_status=$(aws personalize describe-dataset-import-job \
+                --dataset-import-job-arn ${item_dataset_import_job_arn} | jq '.datasetImportJob.status' -r)
+    interaction_dataset_import_job_status=$(aws personalize describe-dataset-import-job \
+                --dataset-import-job-arn ${interaction_dataset_import_job_arn} | jq '.datasetImportJob.status' -r)
 
-#     echo "user_dataset_import_job_status: ${user_dataset_import_job_status}"
-#     echo "item_dataset_import_job_status: ${item_dataset_import_job_status}"
-#     echo "interaction_dataset_import_job_status: ${interaction_dataset_import_job_status}"
+    echo "user_dataset_import_job_status: ${user_dataset_import_job_status}"
+    echo "item_dataset_import_job_status: ${item_dataset_import_job_status}"
+    echo "interaction_dataset_import_job_status: ${interaction_dataset_import_job_status}"
     
-#     if [[ $user_dataset_import_job_status = "CREATE FAILED" || $item_dataset_import_job_status = "CREATE FAILED" || $interaction_dataset_import_job_status = "CREATE FAILED" ]]
-#     then
-#         echo "!!!Dataset Import Job Failed!!!"
-#         echo "!!!Personalize Service Create Failed!!!"
-#         exit 8
-#     elif [[ $user_dataset_import_job_status = "ACTIVE" && $item_dataset_import_job_status = "ACTIVE" && $interaction_dataset_import_job_status = "ACTIVE" ]]
-#     then
-#         echo "Import Job finishing successfully!"
-#         break
-#     fi
+    if [[ "$user_dataset_import_job_status" = "CREATE FAILED" || "$item_dataset_import_job_status" = "CREATE FAILED" || "$interaction_dataset_import_job_status" = "CREATE FAILED" ]]
+    then
+        echo "!!!Dataset Import Job Failed!!!"
+        echo "!!!Personalize Service Create Failed!!!"
+        exit 8
+    elif [[ "$user_dataset_import_job_status" = "ACTIVE" && "$item_dataset_import_job_status" = "ACTIVE" && "$interaction_dataset_import_job_status" = "ACTIVE" ]]
+    then
+        echo "Import Job finishing successfully!"
+        break
+    fi
     
-#     CURRENT_TIME=`expr ${CURRENT_TIME} + 60`
-#     echo "wait for 1 min..."
-#     sleep 60
+    CURRENT_TIME=`expr ${CURRENT_TIME} + 60`
+    echo "wait for 1 min..."
+    sleep 60
 
-# done
+done
 
-# if [ $CURRENT_TIME -ge $MAX_TIME ]
-# then
-#     echo "Import Job Time exceed 10 min, please delete import job and try again!"
-#     exit 8
-# fi
+if [ $CURRENT_TIME -ge $MAX_TIME ]
+then
+    echo "Import Job Time exceed 10 min, please delete import job and try again!"
+    exit 8
+fi
 
 
 #create solution
@@ -166,12 +166,12 @@ userPersonalize_solution_arn=$(aws personalize create-solution \
 
 #monitor solution
 echo "Solution Creating... It will takes no longer than 10 min..."
-MAX_TIME=10*60 # 10 min
+MAX_TIME=`expr 10 \* 60` # 10 min
 CURRENT_TIME=0
 while(( ${CURRENT_TIME} < ${MAX_TIME} )) 
 do
     userPersonalize_solution_status=$(aws personalize describe-solution \
-        --solution-arn ${userPersonalize_solution_arn} | jq '.solution.status')
+        --solution-arn ${userPersonalize_solution_arn} | jq '.solution.status' -r)
     
     echo "userPersonalize_solution_status: ${userPersonalize_solution_status}"
     
@@ -180,7 +180,7 @@ do
         echo "!!!UserPersonalize Solution Create Failed!!!"
         echo "!!!Personalize Service Create Failed!!!"
         exit 8
-    elif [ "${userPersonalize_solution_status}" = "ACTIVE" ]
+    elif [ "$userPersonalize_solution_status" = "ACTIVE" ]
     then
         echo "UserPersonalize Solution  create successfully!"
         break;
@@ -206,12 +206,12 @@ userPersonalize_solution_version_arn=$(aws personalize create-solution-version \
 
 #monitor solution version
 echo "Solution Version Creating... It will takes no longer than 6 hours..."
-MAX_TIME=6*60*60 # 6 hours
+MAX_TIME=`expr 6 \* 60 \* 60` # 6 hours
 CURRENT_TIME=0
 while(( ${CURRENT_TIME} < ${MAX_TIME} )) 
 do
     userPersonalize_solution_version_status=$(aws personalize describe-solution-version \
-            --solution-version-arn ${userPersonalize_solution_version_arn} | jq '.solutionVersion.status')
+            --solution-version-arn ${userPersonalize_solution_version_arn} | jq '.solutionVersion.status' -r)
             
     echo "userPersonalize_solution_version_status: ${userPersonalize_solution_version_status}"
     
@@ -238,6 +238,20 @@ then
 fi
 
 
+# create event tracker
+eventTrackerArn=$(aws personalize create-event-tracker \
+    --name NewsEventTracker \
+    --dataset-group-arn ${datasetGroupArn} | jq '.eventTrackerArn' -r)
+
+trackingId=$(aws personalize describe-event-tracker \
+    --event-tracker-arn ${eventTrackerArn} | jq '.eventTracker.trackingId' -r)
+    
+echo "eventTrackerArn: ${eventTrackerArn}"
+echo "trackingId: ${trackingId}"
+
+# # for test
+# userPersonalize_solution_version_arn="arn:aws:personalize:ap-northeast-1:466154167985:solution/userPersonalizeSolution/e98f9f3c"
+
 #print metrics
 echo "UserPersonalize Solution Metrics:"
 aws personalize get-solution-metrics --solution-version-arn ${userPersonalize_solution_version_arn}
@@ -252,12 +266,12 @@ userPersonalize_campaign_arn=$(aws personalize create-campaign \
 
 #monitor campaign
 echo "Campaign Creating... It will takes no longer than 3 hours..."
-MAX_TIME=3*60*60 # 6 hours
+MAX_TIME=`expr 3 \* 60 \* 60` # 3 hours
 CURRENT_TIME=0
 while(( ${CURRENT_TIME} < ${MAX_TIME} )) 
 do
     userPersonalize_campaign_status=$(aws personalize describe-campaign \
-            --campaign-arn ${userPersonalize_campaign_arn} | jq '.campaign.status')
+            --campaign-arn ${userPersonalize_campaign_arn} | jq '.campaign.status' -r)
             
     echo "userPersonalize_campaign_status: ${userPersonalize_campaign_status}"
     
