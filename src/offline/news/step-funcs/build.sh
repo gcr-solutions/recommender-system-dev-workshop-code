02188b8032d5fa82e90d22f440a3a3a66a47bf6a
liuyong <23968560@qq.com>
2021-07-06 16:57:31 +0800

    (steps|item-new-assembled)

--- src/offline/news/step-funcs/build.sh
+++ src/offline/news/step-funcs/build.sh
@@ -59,7 +59,7 @@ do
     echo "----"
     echo "STACK_NAME: ${STACK_NAME}"
     echo "template_file: ${template_file}"
-    if [[ $name == 'steps' && $REGION =~ ^cn.* ]]; then
+    if [[ $name =~ ^(steps|item-new-assembled)$ && $REGION =~ ^cn.* ]]; then
       org_template_file=${template_file}
       sed 's#.amazonaws.com/#.amazonaws.com.cn/#g' ${template_file} > tmp_1_${org_template_file}
       sed 's#522244679887.dkr.ecr.ap-northeast-1#'${AWS_ACCOUNT_ID}'.dkr.ecr.'${REGION}'#g' tmp_1_${org_template_file}  > tmp_2_${org_template_file}
@@ -81,7 +81,7 @@ do
          exit 1
      fi
 
-     if [[ $name == 'steps' && $REGION =~ ^cn.* ]]; then
+     if [[ $name =~ ^(steps|item-new-assembled)$ && $REGION =~ ^cn.* ]]; then
        rm tmp_*.yaml
      fi
 
