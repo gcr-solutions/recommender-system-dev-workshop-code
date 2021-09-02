2a5ca5bea42be81ad31bbabc804be1dfe8ff386e
amliuyong <am.liuyong@qq.com>
2021-09-02 15:28:56 +0800

    SsmParameterValueForImageId

--- scripts/cdk/sync_s3.sh
+++ scripts/cdk/sync_s3.sh
@@ -4,4 +4,3 @@ aws --profile rsops s3 cp ./rs-raw-ec2.yaml s3://aws-gcr-rs-sol-workshop-ap-nort
 sed -i -e 's/SsmParameterValueawsserviceamiamazonlinuxlatestamzn2ami.*Parameter/SsmParameterValueForImageId/g' ./rs-raw-ec2.yaml
 echo 'https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/rs-raw-ec2.yaml'
 rm ./rs-raw-ec2.yaml-e || true
-
