66623a84333e98f7d0e8bf467069ca463df3fd61
amliuyong <am.liuyong@qq.com>
2021-09-02 14:55:27 +0800

    fix config aws env

--- scripts/cdk/lib/config/rs-raw-ec2-user-data.sh
+++ scripts/cdk/lib/config/rs-raw-ec2-user-data.sh
@@ -23,14 +23,15 @@ systemctl enable httpd
 echo "<h1>Hello World from AWS EC2 $(hostname -f)</h1><br><hr><h2>Start Time: $(date -'u' )</h2>" > /var/www/html/index.html
 
 # config AWS ENV
-su - ec2-user
-whoami
 
+sudo -u ec2-user -i <<EOS
+echo "set default.region"
 AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
 ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
-mkidr ~/.aws || true
 aws configure set default.region ${AWS_REGION}
 aws configure get default.region
 echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
 echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
 echo "export REGION=${AWS_REGION}" | tee -a ~/.bash_profile
+EOS
+
