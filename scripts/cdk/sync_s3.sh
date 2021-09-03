1bb267d9986f37a55701b4358949c2462ed2df30
amliuyong <am.liuyong@qq.com>
2021-09-03 17:48:13 +0800

    add StableVersion

--- scripts/cdk/sync_s3.sh
+++ scripts/cdk/sync_s3.sh
@@ -2,27 +2,31 @@
 
 set -e 
 
-Stable=$1
+StableVersion=$1
 
 AWS_CMD="aws --profile rsops"
 
 cdk synth RsRawEC2CdkStack > rs-raw-ec2.yaml
 sed -i -e 's/SsmParameterValueawsserviceamiamazonlinuxlatestamzn2ami.*Parameter/SsmParameterValueForImageId/g' ./rs-raw-ec2.yaml
-$AWS_CMD s3 cp ./rs-raw-ec2.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/ --acl public-read
-rm ./rs-raw-ec2.yaml-e || true
+$AWS_CMD s3 cp ./rs-raw-ec2.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/rs-dev-workshop-code/latest/ --acl public-read
+
 
 rm main.zip > /dev/null 2>&1 || true
-echo 'https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/rs-raw-ec2.yaml'
+echo 'https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/rs-dev-workshop-code/latest/rs-raw-ec2.yaml'
 
 wget https://github.com/gcr-solutions/recommender-system-dev-workshop-code/archive/refs/heads/main.zip
 
 $AWS_CMD s3 cp main.zip  s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/rs-dev-workshop-code/latest/ --acl public-read
 $AWS_CMD s3 cp main.zip  s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/rs-dev-workshop-code/$(date +"%Y-%m-%d")/ --acl public-read
 
-if [[ $Stable -eq 1 ]]; then
+if [[ $StableVersion == 'v1' ]]; then
     echo "copy to stable_v1"
+    sed -i -e 's#rs-dev-workshop-code/latest/main.zip#rs-dev-workshop-code/stable_v1/main.zip' ./rs-raw-ec2.yaml
     $AWS_CMD s3 cp main.zip s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/rs-dev-workshop-code/stable_v1/ --acl public-read
+    $AWS_CMD s3 cp ./rs-raw-ec2.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/rs-dev-workshop-code/stable_v1/ --acl public-read
+    echo 'https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/rs-dev-workshop-code/stable_v1/rs-raw-ec2.yaml'
 fi 
 
+rm ./rs-raw-ec2.yaml-e || true
 rm main.zip
 
