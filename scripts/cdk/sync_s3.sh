dec8d16139d0905f89c1ecd0ec5ea22498c85e19
amliuyong <am.liuyong@qq.com>
2021-09-03 18:20:48 +0800

    add todayStr

--- scripts/cdk/sync_s3.sh
+++ scripts/cdk/sync_s3.sh
@@ -6,25 +6,34 @@ StableVersion=$1
 
 AWS_CMD="aws --profile rsops"
 
+todayStr=$(date +"%Y-%m-%d")
+bucket=aws-gcr-rs-sol-workshop-ap-northeast-1-common
+
 cdk synth RsRawEC2CdkStack > rs-raw-ec2.yaml
 sed -i -e 's/SsmParameterValueawsserviceamiamazonlinuxlatestamzn2ami.*Parameter/SsmParameterValueForImageId/g' ./rs-raw-ec2.yaml
-$AWS_CMD s3 cp ./rs-raw-ec2.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/rs-dev-workshop-code/latest/ --acl public-read
 
+$AWS_CMD s3 cp ./rs-raw-ec2.yaml s3://${bucket}/rs-dev-workshop-code/latest/ --acl public-read
+echo 'https://${bucket}.s3.ap-northeast-1.amazonaws.com/rs-dev-workshop-code/latest/rs-raw-ec2.yaml'
+
+sed -e 's#rs-dev-workshop-code/latest/main.zip#rs-dev-workshop-code/${todayStr}/main.zip#g' ./rs-raw-ec2.yaml > ./${todayStr}-rs-raw-ec2.yaml
+$AWS_CMD s3 cp ./${todayStr}-rs-raw-ec2.yaml s3://${bucket}/rs-dev-workshop-code/${todayStr}/rs-raw-ec2.yaml --acl public-read
+echo 'https://${bucket}.s3.ap-northeast-1.amazonaws.com/rs-dev-workshop-code/${todayStr}/rs-raw-ec2.yaml'
+
+rm ${todayStr}-rs-raw-ec2.yaml
 
 rm main.zip > /dev/null 2>&1 || true
-echo 'https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/rs-dev-workshop-code/latest/rs-raw-ec2.yaml'
 
 wget https://github.com/gcr-solutions/recommender-system-dev-workshop-code/archive/refs/heads/main.zip
 
-$AWS_CMD s3 cp main.zip  s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/rs-dev-workshop-code/latest/ --acl public-read
-$AWS_CMD s3 cp main.zip  s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/rs-dev-workshop-code/$(date +"%Y-%m-%d")/ --acl public-read
+$AWS_CMD s3 cp main.zip  s3://${bucket}/rs-dev-workshop-code/latest/ --acl public-read
+$AWS_CMD s3 cp main.zip  s3://${bucket}/rs-dev-workshop-code/${todayStr}/ --acl public-read
 
 if [[ $StableVersion == 'v1' ]]; then
     echo "copy to stable_v1"
     sed -i -e 's#rs-dev-workshop-code/latest/main.zip#rs-dev-workshop-code/stable_v1/main.zip#g' ./rs-raw-ec2.yaml
-    $AWS_CMD s3 cp main.zip s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/rs-dev-workshop-code/stable_v1/ --acl public-read
-    $AWS_CMD s3 cp ./rs-raw-ec2.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/rs-dev-workshop-code/stable_v1/ --acl public-read
-    echo 'https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/rs-dev-workshop-code/stable_v1/rs-raw-ec2.yaml'
+    $AWS_CMD s3 cp main.zip s3://${bucket}/rs-dev-workshop-code/stable_v1/ --acl public-read
+    $AWS_CMD s3 cp ./rs-raw-ec2.yaml s3://${bucket}/rs-dev-workshop-code/stable_v1/ --acl public-read
+    echo 'https://${bucket}.s3.ap-northeast-1.amazonaws.com/rs-dev-workshop-code/stable_v1/rs-raw-ec2.yaml'
 fi 
 
 rm main.zip
