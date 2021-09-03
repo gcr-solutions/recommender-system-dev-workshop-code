2158160b09a23472426c202b3879b3859a5824f6
amliuyong <am.liuyong@qq.com>
2021-09-03 13:49:08 +0800

    add step-funcs and lambda to codebuild

--- scripts/create-offline.sh
+++ scripts/create-offline.sh
@@ -46,22 +46,23 @@ echo "2. ========= sync sample data to S3 =============="
 cd ${curr_dir}/../sample-data
 ./sync_data_to_s3.sh $Stage
 
-echo "3. ========= Build lambda =============="
-cd ${curr_dir}/../src/offline/lambda
-./build.sh $Stage
+#echo "3. ========= Build lambda =============="
+#cd ${curr_dir}/../src/offline/lambda
+#./build.sh $Stage
+#
+#echo "4. ========= Build stepfuncs =============="
+#cd ${curr_dir}/../src/offline/news/step-funcs
+#./build.sh $Stage
 
-echo "4. ========= Build stepfuncs =============="
-cd ${curr_dir}/../src/offline/news/step-funcs
-./build.sh $Stage
+#echo "Offline resources are created successfully"
+#echo "You can run your step-funcs with below input"
+#
+#echo '{
+#  "Bucket": "aws-gcr-rs-sol-'${Stage}'-'${REGION}'-'${AWS_ACCOUNT_ID}'",
+#  "S3Prefix": "sample-data-news",
+#  "change_type": "ITEM|BATCH|USER|MODEL"
+#}'
 
-echo "Offline resources are created successfully"
-echo "You can run your step-funcs with below input"
-
-echo '{
-  "Bucket": "aws-gcr-rs-sol-'${Stage}'-'${REGION}'-'${AWS_ACCOUNT_ID}'",
-  "S3Prefix": "sample-data-news",
-  "change_type": "ITEM|BATCH|USER|MODEL"
-}'
 echo ""
 
 
