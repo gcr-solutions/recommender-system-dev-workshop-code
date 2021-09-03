4f9843cd581282a49c57ae55ddb5401048eddd9d
amliuyong <am.liuyong@qq.com>
2021-09-03 13:45:56 +0800

    change to codecommit

--- scripts/create-offline.sh
+++ scripts/create-offline.sh
@@ -40,7 +40,7 @@ sleep 3
 
 echo "1. ========= Create codebuild =============="
 cd ${curr_dir}/codebuild
-./register-to-codebuild-offline.sh $Stage
+./register-to-codebuild-offline-codecommit.sh $Stage
 
 echo "2. ========= sync sample data to S3 =============="
 cd ${curr_dir}/../sample-data
