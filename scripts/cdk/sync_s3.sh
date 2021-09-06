9739bb9f4fae89ae27e3bfbeff4af58c098b783d
amliuyong <am.liuyong@qq.com>
2021-09-06 09:43:13 +0800

    add github-rs-raw-ec2.yaml

--- scripts/cdk/sync_s3.sh
+++ scripts/cdk/sync_s3.sh
@@ -15,11 +15,15 @@ sed -i -e 's/SsmParameterValueawsserviceamiamazonlinuxlatestamzn2ami.*Parameter/
 $AWS_CMD s3 cp ./rs-raw-ec2.yaml s3://${bucket}/rs-dev-workshop-code/latest/ --acl public-read > /tmp/rs-run.log 2>&1
 echo "https://${bucket}.s3.ap-northeast-1.amazonaws.com/rs-dev-workshop-code/latest/rs-raw-ec2.yaml"
 
-sed -e "s#rs-dev-workshop-code/latest/main.zip#rs-dev-workshop-code/${todayStr}/main.zip#g" ./rs-raw-ec2.yaml >> ./${todayStr}-rs-raw-ec2.yaml
+sed -e "s#rs-dev-workshop-code/latest/main.zip#rs-dev-workshop-code/${todayStr}/main.zip#g" ./rs-raw-ec2.yaml > ./${todayStr}-rs-raw-ec2.yaml
 $AWS_CMD s3 cp ./${todayStr}-rs-raw-ec2.yaml s3://${bucket}/rs-dev-workshop-code/${todayStr}/rs-raw-ec2.yaml --acl public-read >>/tmp/rs-run.log 2>&1
 echo "https://${bucket}.s3.ap-northeast-1.amazonaws.com/rs-dev-workshop-code/${todayStr}/rs-raw-ec2.yaml"
-
 rm ${todayStr}-rs-raw-ec2.yaml
+
+sed -e "s#https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/rs-dev-workshop-code/latest/main.zip#https://github.com/gcr-solutions/recommender-system-dev-workshop-code/archive/refs/heads/main.zip#" > ./github-rs-raw-ec2.yaml
+$AWS_CMD s3 cp ./github-rs-raw-ec2.yaml s3://${bucket}/rs-dev-workshop-code/github/rs-raw-ec2.yaml --acl public-read >>/tmp/rs-run.log 2>&1
+echo "https://${bucket}.s3.ap-northeast-1.amazonaws.com/rs-dev-workshop-code/github/rs-raw-ec2.yaml"
+rm github-rs-raw-ec2.yaml
 
 rm main.zip > /dev/null 2>&1 || true
 
