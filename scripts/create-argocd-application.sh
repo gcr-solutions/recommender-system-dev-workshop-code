845f83d4ddcba5ff1afe87db725f9df9540a4d31
AoyuQC <aoyuzhang1989@gmail.com>
2021-09-07 17:15:32 +0800

    fix bug for s3

--- scripts/create-argocd-application.sh
+++ scripts/create-argocd-application.sh
@@ -18,7 +18,12 @@ echo "update-lambda-env"
 ./update-lambda-env.sh
 
 # 3 Create argocd application
-argocd app create gcr-recommender-system-news-dev --repo https://${ACCESS_TOKEN}@github.com/${GITHUB_USER}/recommender-system-dev-workshop-code.git --path manifests/envs/news-dev --dest-namespace \
+USER_NAME="gcr-rs-codecommit-user-at-002224604296"
+PASS_WRD="nuZYIyXHfA2W+qwoLxto4Q3Q1oHwMDsnh+ebquz9Y9E="
+REPO = "https://git-codecommit.ap-southeast-1.amazonaws.com/v1/repos/recommender-system-dev-workshop-code"
+argocd repo add $REPO --username $USER_NAME --password $PASS_WRD --insecure-skip-server-verification
+
+argocd app create gcr-recommender-system-news-dev --repo $REPO --path manifests/envs/news-dev --dest-namespace \
 rs-news-dev-ns --dest-server https://kubernetes.default.svc --kustomize-image gcr.io/heptio-images/ks-guestbook-demo:0.1
 
 sleep 20
