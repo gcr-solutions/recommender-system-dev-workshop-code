1f2574e8fe1ab79da4185a650941258fbd5cbf95
amliuyong <am.liuyong@qq.com>
2021-09-07 15:29:51 +0800

    codebuild role cf

--- scripts/codebuild/create-iam-role.sh
+++ scripts/codebuild/create-iam-role.sh
@@ -29,107 +29,41 @@ if [[ -z $REGION ]];then
 fi
 
 AWS_P='aws'
-
 if [[ $REGION =~ cn.* ]];then
   AWS_P='aws-cn'
 fi
 
 AWS_ACCOUNT_ID=$(${AWS_CMD} sts get-caller-identity --region ${REGION} --query Account --output text)
-
 echo "AWS_ACCOUNT_ID: $AWS_ACCOUNT_ID"
-
-ROLE_NAME=gcr-rs-${Stage}-codebuild-role
-ROLE_POLICY=gcr-rs-${Stage}-codebuild-policy
-
+ROLE_NAME=gcr-rs-${Stage}-codebuild-role-${REGION}
 echo "ROLE_NAME:$ROLE_NAME"
-echo "ROLE_POLICY:$ROLE_POLICY"
-
-function del_role() {
-  ${AWS_CMD} iam list-attached-role-policies --role-name ${ROLE_NAME} | jq -r '.AttachedPolicies[].PolicyArn'
-  for policyArn in $(${AWS_CMD} iam list-attached-role-policies --role-name ${ROLE_NAME} | jq -r '.AttachedPolicies[].PolicyArn'); do
-    ${AWS_CMD} iam detach-role-policy \
-      --role-name ${ROLE_NAME} \
-      --policy-arn ${policyArn} || true
-    echo "Detached ${ROLE_NAME} and ${policyArn}"
-
-    count=$(${AWS_CMD} iam list-policy-versions --policy-arn ${policyArn} | jq -r '.Versions[].VersionId' | wc -l)
-    if [ $count -eq '1' ]; then
-      ${AWS_CMD} iam delete-policy --policy-arn ${policyArn} || true
-    else
-      for versionId in $(${AWS_CMD} iam list-policy-versions --policy-arn ${policyArn} | jq -r '.Versions[].VersionId'); do
-        ${AWS_CMD} iam delete-policy-version \
-          --policy-arn ${policyArn} \
-          --version-id ${versionId}
-        echo "Deleted ${ROLE_POLICY} = ${policyArn} : ${versionId}" || true
-      done
-      ${AWS_CMD} iam delete-policy --policy-arn ${policyArn}
-    fi
-  done
-
-  ${AWS_CMD} iam delete-role --role-name ${ROLE_NAME} || true
-  if [ $? -ne 0 ]; then
-     echo "Failed to delete role : exit 0"
-     exit 1
-  fi
-  echo "Deleted ${ROLE_NAME}"
-}
-
-if [[ $paramDelete == 'DELETE' ]]; then
-    echo "Start to DELETE codebuild role"
-    del_role
-    exit 0
-fi 
-
-echo "Start to create codebuild role"
-ROLE_NAMES=$(${AWS_CMD} iam list-roles | jq '.[][] | select(.RoleName=="'${ROLE_NAME}'")')
-if [ "$ROLE_NAMES" == "" ]; then
-  echo "Nothing has been done and all clear."
-else
-  echo "Delete role"
-  del_role
-fi
-
-echo ""
-
-${AWS_CMD} iam delete-role --role-name ${ROLE_NAME} >/dev/null 2>&1 || true
-echo "${AWS_CMD} iam create-role \
-  --role-name ${ROLE_NAME} \
-  --assume-role-policy-document file://assume-role.json"
-
-${AWS_CMD} iam create-role \
-  --role-name ${ROLE_NAME} \
-  --assume-role-policy-document file://assume-role.json || true
 
 roleArn="arn:${AWS_P}:iam::${AWS_ACCOUNT_ID}:role/${ROLE_NAME}"
+echo "roleArn:${roleArn}"
 
-echo "Created ${ROLE_NAME} = ${roleArn}"
-echo ""
+STACK_NAME=rs-$Stage-codebuild-role-stack
+echo "STACK_NAME: ${STACK_NAME}"
 
-${AWS_CMD} iam delete-policy --policy-arn ${policyArn} > /dev/null 2>&1 || true
-echo "${AWS_CMD} iam create-policy \
-  --policy-name ${ROLE_POLICY} \
-  --policy-document file://iam-role-policy.json | jq -r '.Policy.Arn'"
-
-${AWS_CMD} iam create-policy \
-  --policy-name ${ROLE_POLICY} \
-  --policy-document file://iam-role-policy.json || true
-
-rolePolicyArn="arn:${AWS_P}:iam::${AWS_ACCOUNT_ID}:policy/${ROLE_POLICY}"
-
-echo ""
-echo "${AWS_CMD} iam attach-role-policy \
-  --role-name ${ROLE_NAME} \
-  --policy-arn ${rolePolicyArn}"
-
-${AWS_CMD} iam attach-role-policy \
-  --role-name ${ROLE_NAME} \
-  --policy-arn ${rolePolicyArn} || true
-
-echo "Atteched ${rolePolicyArn} to ${ROLE_NAME} "
-if [ $? -ne 0 ]; then
-  echo "Failed to create role : exit 0"
+if [[ -n $paramDelete == 'DELETE' ]]; then
+  echo "Clean STACK_NAME: ${STACK_NAME}"
+  $AWS_CMD cloudformation delete-stack --region ${REGION} --stack-name ${STACK_NAME}
   exit 0
 fi
 
+PARAMETER_OVERRIDES="Stage=$Stage"
+$AWS_CMD  cloudformation deploy --region ${REGION} \
+--template-file ./codebuild-role.yaml --stack-name ${STACK_NAME} \
+--parameter-overrides ${PARAMETER_OVERRIDES} \
+--capabilities CAPABILITY_NAMED_IAM
+
+StackStatus=$($AWS_CMD cloudformation  describe-stacks --region ${REGION} --stack-name ${STACK_NAME} --output table | grep StackStatus)
+echo ${StackStatus} |  egrep "(CREATE_COMPLETE)|(UPDATE_COMPLETE)" > /dev/null
+
+if [[ $? -ne 0 ]]; then
+     echo "error!!!  ${StackStatus}"
+     exit 1
+fi
+
 echo ${roleArn} >_role.arn
+cat _role.arn
 echo "Create codebuild role successfully"
