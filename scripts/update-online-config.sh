a193c12371cad6bc6d304509c1587d65cd43a656
AoyuQC <aoyuzhang1989@gmail.com>
2021-09-08 14:11:58 +0800

    fix config bug

--- scripts/update-online-config.sh
+++ scripts/update-online-config.sh
@@ -2,21 +2,22 @@
 set -e
 
 # 1 update redis config
-REDIS_ENDPOINT=$(aws elasticache describe-cache-clusters --cache-cluster-id gcr-rs-dev-workshop-redis-cluster --show-cache-node-info \
---query "CacheClusters[].CacheNodes[].Endpoint.Address" --output text)
-echo $REDIS_ENDPOINT
+
+while true; do
+  REDIS_ENDPOINT=$(aws elasticache describe-cache-clusters --cache-cluster-id gcr-rs-dev-workshop-redis-cluster --show-cache-node-info \
+  --query "CacheClusters[].CacheNodes[].Endpoint.Address" --output text)
+  if [ "$REDIS_ENDPOINT" == "" ]; then
+    echo "wait redis endpoint to create!"
+  else
+    echo "redis create complete!"
+    echo $REDIS_ENDPOINT
+    break
+  fi
+  sleep 10
+done
+
 cd ../manifests/envs/news-dev
 cat config-template.yaml | sed 's/REDIS_HOST_PLACEHOLDER/'"$REDIS_ENDPOINT"'/g' > config_1.yaml
-
-# 2 update kubernetes config map
-if [[ -z $REGION ]];then
-    REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
-fi
-
-if [[ $? -ne 0 ]]; then
-  echo "error!!! can not get your AWS_ACCOUNT_ID"
-  exit 1
-fi
 
 echo "REGION: $REGION"
 echo "ACCOUNT_ID: $ACCOUNT_ID"
