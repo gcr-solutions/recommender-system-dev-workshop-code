f2317d888685cce8d190c56dc96d7cbee32046d7
amliuyong <am.liuyong@qq.com>
2021-09-09 15:50:24 +0800

    fix link for cn

--- scripts/setup-argocd-server.sh
+++ scripts/setup-argocd-server.sh
@@ -6,18 +6,33 @@ export EKS_CLUSTER=gcr-rs-dev-application-cluster
 echo $EKS_CLUSTER
 
 # 1 setup argocd server
-kubectl create namespace argocd
+kubectl create namespace argocd || true
 
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+install_link="https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/eks/argocd/install.yaml"
+if [[ $REGION =~ cn.* ]];then
+  install_link="https://aws-gcr-rs-sol-workshop-cn-north-1-common.s3.cn-north-1.amazonaws.com.cn/eks/argocd/install.yaml"
+fi
+
+echo "1. $install_link"
+kubectl apply -n argocd -f $install_link
 
 sleep 10
 
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
 # 2 install argocd cli
-VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+#VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
 
-sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
+install_link="https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/eks/argocd/argocd-linux-amd64-v2.1.2"
+if [[ $REGION =~ cn.* ]];then
+  install_link="https://aws-gcr-rs-sol-workshop-cn-north-1-common.s3.cn-north-1.amazonaws.com.cn/eks/argocd/argocd-linux-amd64-v2.1.2"
+fi
+
+echo "2. $install_link"
+
+rm -rf /usr/local/bin/argocd > /dev/null 2>&1 || true
+
+sudo curl -sSL -o /usr/local/bin/argocd $install_link
 
 sudo chmod +x /usr/local/bin/argocd
 
@@ -30,5 +45,5 @@ dns_name=$(kubectl get svc argocd-server -n argocd -o=jsonpath='{.status.loadBal
 
 echo user name: admin
 echo password: $ARGOCD_PASSWORD
-echo endpoint: $dns_name
+echo endpoint: http://$dns_name
 
