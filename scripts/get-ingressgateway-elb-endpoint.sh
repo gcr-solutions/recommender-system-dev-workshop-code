9d18051fb712a3c38f6a300aa836b6c86a05aa95
amliuyong <am.liuyong@qq.com>
2021-09-13 16:12:50 +0800

    add add ssh tunnel

--- scripts/get-ingressgateway-elb-endpoint.sh
+++ scripts/get-ingressgateway-elb-endpoint.sh
@@ -8,7 +8,7 @@ echo "-------"
 if [[ $REGION =~ cn.* ]];then
   EC2_IP=$(curl -s 169.254.169.254/latest/meta-data/public-ipv4)
   # dns_name=$(kubectl get svc istio-ingressgateway-news-dev -n istio-system -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}')
-  echo "Please create ssh tunnel on your localhost:"
+  echo "Please create SSH tunnel on your localhost:"
   echo ""
   echo "ssh -L 8181:$dns_name:22 -i gcr-rs-dev-workshop-ec2-key.pem  ec2-user@$EC2_IP"
   echo ""
