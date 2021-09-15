ba3c3532c1c2cf393921747cf4e2f38311d8b8a9
jjin-2019 <lijin2019@hotmail.com>
2021-09-15 22:49:27 +0800

    update for cn

--- src/offline/news/ps-sims/item-feature-update-batch/build.sh
+++ src/offline/news/ps-sims/item-feature-update-batch/build.sh
@@ -4,21 +4,55 @@ set -e
 echo "------------------------------------------------ "
 Stage=$1
 if [[ -z $Stage ]];then
-  Stage='dev'
+  Stage='dev-workshop'
 fi
 
-echo "Stage=$Stage"
+if [[ -n $REGION ]];then
+  AWS_REGION=$REGION
+fi
+
+if [[ -z $AWS_REGION ]];then
+  AWS_REGION='ap-northeast-1'
+fi
+
+echo "Stage=$Stage, AWS_REGION=$AWS_REGION"
 
 repoName=rs/news-ps-sims-item-feature-update-batch
+if [[ -n $REPO_NAME ]];then
+  repoName=$REPO_NAME
+fi
+
+
+function download_dgl() {
+    url=$1
+    echo "download:$url"
+    rm -rf ./dgl > /dev/null  2>&1
+    mkdir ./dgl  && cd ./dgl
+    wget --quiet $url || {
+        echo "error: fail to download $url"
+        exit 1
+    }
+    unzip dgl-ke.zip
+    rm dgl-ke.zip
+    cd ..
+}
+
+dgl_url=https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/dgl/dgl-ke.zip
+
 
 if [[ $Stage == 'demo' ]]; then
     ../dev2demo.sh $repoName
 else
     rm -rf fasthan_base >/dev/null 2>&1
     mkdir fasthan_base
-    if [[ $REGION =~ ^cn.* ]]; then
+    if [[ $AWS_REGION =~ ^cn.* ]]; then
+      dgl_url=https://aws-gcr-solutions-assets.s3.cn-northwest-1.amazonaws.com.cn/gcr-rs/dgl/dgl-ke.zip
+
       cd ./fasthan_base
-      wget https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/fasthan_base.zip
+      wget --quiet https://aws-gcr-solutions-assets.s3.cn-northwest-1.amazonaws.com.cn/gcr-rs/fasthan/fasthan_base.zip || {
+        echo "error: fail to download fasthan_base.zip"
+        exit 1
+       }
     else
       aws s3 cp s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/fasthan_base.zip ./fasthan_base
       cd fasthan_base
@@ -26,6 +60,9 @@ else
     unzip fasthan_base.zip
     rm fasthan_base.zip
     cd ..
+
+    # install dgl-ke.zip
+    download_dgl $dgl_url
 
     ../norm_build.sh $repoName $Stage
 
