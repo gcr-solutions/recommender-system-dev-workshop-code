9ebc598000d9a9bff1136f6f27f8d5b961e735fe
jjin-2019 <lijin2019@hotmail.com>
2021-11-02 14:46:24 +0800

    fix method typo

--- scripts/update-online-config.sh
+++ scripts/update-online-config.sh
@@ -7,6 +7,10 @@ if [[ -z $SCENARIO ]]; then
   SCENARIO='news'
 fi
 
+if [[ -z $METHOD ]]; then
+  METHOD='customize'
+fi
+
 while true; do
   REDIS_ENDPOINT=$(aws elasticache describe-cache-clusters --cache-cluster-id gcr-rs-dev-workshop-redis-cluster --show-cache-node-info \
   --query "CacheClusters[].CacheNodes[].Endpoint.Address" --output text)
@@ -25,7 +29,7 @@ cd ../manifests/envs/${SCENARIO}-dev
 echo "REGION: $REGION"
 echo "ACCOUNT_ID: $ACCOUNT_ID"
 
-sed -e "s|REDIS_HOST_PLACEHOLDER|$REDIS_ENDPOINT|g;s|__AWS_REGION__|$REGION|g;s|__AWS_ACCOUNT_ID__|$ACCOUNT_ID|g;s|__SCENARIO__|$SCENARIO|g"  \
+sed -e "s|REDIS_HOST_PLACEHOLDER|$REDIS_ENDPOINT|g;s|__AWS_REGION__|$REGION|g;s|__AWS_ACCOUNT_ID__|$ACCOUNT_ID|g;s|__METHOD__|$METHOD|g"  \
             ./config-template.yaml > config.yaml
 
 
